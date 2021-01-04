d7ef91669fff165f833b2e6a77b2b64513886a35
zitssu <74772094+zitssu@users.noreply.github.com>
2021-01-04 20:55:28 +0100

    Create badr.c

---
+++ badr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+using namespace std;
+
+int main(const_*char)
+{
+
+
+    char mayArray[] = { "R" , "V" , "J" , "O" };
+    char myArray[] = { x1 , x2 , x3 , x4 };
+
+    printf("/n", "entrer 4 valeur sans espace");
+    scanf("%d", &myArray[0]);
+    scanf("%d", &myArray[1]);
+    scanf("%d", &myArray[2]);
+    scanf("%d", &myArray[3]);
+
+    if (x1 != "R")
+    {
+        printf
+    }
+}
